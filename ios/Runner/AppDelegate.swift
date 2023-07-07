f7ae25639be4b0abdd4eabe9b4da9f7eec48f559
IngerLisePaulsen <ingerlisepau@hotmail.com>
2023-07-07 15:15:06 +0200

    upgrade

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyBQUg7Kjg9bBNBUdxD39QQZCdXEA0dbb6E")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
