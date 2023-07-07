69a6919c76a8a64212f2d7f13f20bb98dcb9d09a
IngerLisePaulsen <ingerlisepau@hotmail.com>
2023-07-07 15:14:41 +0200

    upgrade

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:badelove/widgets/current.weather.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -10,6 +11,44 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LatLng {
+}
+
+class GoogleMapController {
+}
+
   final CurrentWeather currentWeather = CurrentWeather(
       cityName: 'Bergen',
       apiKey:
